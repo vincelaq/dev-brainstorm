512bae73c0c73dddf11a81ee03ece814244ecd9b
Chris Ladd <codebyladd@gmail.com>
2021-07-27 21:58:51 -0600

    add new file controllers timelines.js mod timeline.js in routers add HackedIn to header.ejs

--- routes/timeline.js
+++ routes/timeline.js
@@ -1,2 +1,7 @@
+//TimeLine Router
+
 const router = require('express').Router();
 const ctrl = require("../controllers");
+
+//router.get('/', isLoggedIn, ctrl.dashboard.index);
+//router.post('/', isLoggedIn, ctrl.dashboard.create);
