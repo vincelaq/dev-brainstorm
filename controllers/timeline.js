e603f5d5919d28eb385cef4214fd486420924d29
Vince Laquindanum <laq.vince@gmail.com>
2021-07-28 19:08:09 -0700

    Adds functionality to timeline control

---
+++ controllers/timeline.js
@@ -0,0 +1,18 @@
+//TIMELINE CONTROLLER
+const Post = require('../models/Post');
+
+module.exports = {
+    index,
+};
+
+function index (req, res) {
+    Post.find({})
+    .populate('comments', 'user')
+    .exec((err, post) => {
+        console.log(post);
+        if (err) res.send(err);
+        res.render('timeline/index', {
+            post,
+        });
+    })
+};
