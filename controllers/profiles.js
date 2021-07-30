f20f22c6d69fd09ec87d99657eb5d350e5309398
Vince Laquindanum <laq.vince@gmail.com>
2021-07-30 03:05:27 -0700

    Add view other's profile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -4,6 +4,18 @@ const User = require('../models/User');
 module.exports = {
     index,
     update,
+    show,
+};
+
+function show (req, res) {
+    User.findOne({'username': req.params.showId}, (err, one) => {
+        console.log(one);
+        if(err) res.send(err);
+        res.render('profiles/show', {
+            user: req.user,
+            one
+        })
+    })
 };
 
 function update (req, res) {
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const ctrl = require("../controllers");
 
 router.get('/:id', isLoggedIn, ctrl.profiles.index);
+router.get('/show/:showId', isLoggedIn, ctrl.profiles.show);
 router.post('/:id', isLoggedIn, ctrl.profiles.update);
+
 
 function isLoggedIn (req, res, next) {
     if (req.isAuthenticated()) return next();
